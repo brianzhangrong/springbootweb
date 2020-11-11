afd577a5f09edba0e1fd3202b1547336cf739c91
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2020-11-11 14:27:32 +0800

    log

--- src/main/java/com/example/test/springbootweb/dao/TestDao.java
+++ src/main/java/com/example/test/springbootweb/dao/TestDao.java
@@ -2,12 +2,17 @@ package com.example.test.springbootweb.dao;
 
 import com.example.test.springbootweb.config.TestTrace;
 import com.example.test.springbootweb.constant.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TestDao {
+
+    Logger log = LoggerFactory.getLogger("TestDao");
     @TestTrace
     public String version(){
+        log.info("dao {}",Version.VERSION);
         return "testDao-v"+ Version.VERSION;
     }
 }
--- src/main/java/com/example/test/springbootweb/service/TestService.java
+++ src/main/java/com/example/test/springbootweb/service/TestService.java
@@ -4,15 +4,18 @@ import com.example.test.springbootweb.config.TestTrace;
 import com.example.test.springbootweb.constant.Version;
 import com.example.test.springbootweb.dao.TestDao;
 import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
-
+    Logger log = LoggerFactory.getLogger("TestService");
     @Resource
     TestDao testDao;
     @TestTrace
     public String version(){
+        log.info("service {}",Version.VERSION);
         return "serviceV-"+ Version.VERSION+","+testDao.version();
     }
 }
