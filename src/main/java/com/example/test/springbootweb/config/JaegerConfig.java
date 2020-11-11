21aafbdcc244158a923fd7e9b982b106f430aaaf
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2020-11-11 09:36:32 +0800

    7

--- src/main/java/com/example/test/springbootweb/config/JaegerConfig.java
+++ src/main/java/com/example/test/springbootweb/config/JaegerConfig.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JaegerConfig {
 
-//    @Bean
-//    public io.opentracing.Tracer tracer() {
-//        io.jaegertracing.Configuration config = new io.jaegertracing.Configuration("springbootweb");
-//        io.jaegertracing.Configuration.SenderConfiguration sender = new io.jaegertracing.Configuration.SenderConfiguration();
-//        sender.withEndpoint("jaeger-collector.istio-system:14267");
-//        config.withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType("const").withParam(1));
-//        config.withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withSender(sender).withMaxQueueSize(10000));
-//        return config.getTracer();
-//    }
+    @Bean
+    public io.opentracing.Tracer tracer() {
+        io.jaegertracing.Configuration config = new io.jaegertracing.Configuration("springbootweb");
+        io.jaegertracing.Configuration.SenderConfiguration sender = new io.jaegertracing.Configuration.SenderConfiguration();
+        sender.withEndpoint("jaeger-collector.istio-system:14267");
+        config.withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType("const").withParam(1));
+        config.withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withSender(sender).withMaxQueueSize(10000));
+        return config.getTracer();
+    }
 
 }
