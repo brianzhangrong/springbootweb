86b235398b74ab33f4b44eb62d951e0334c94916
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2020-11-11 08:38:33 +0800

    jaeger

--- src/main/java/com/example/test/springbootweb/controller/Testcontroller.java
+++ src/main/java/com/example/test/springbootweb/controller/Testcontroller.java
@@ -1,13 +1,25 @@
 package com.example.test.springbootweb.controller;
 
+
+
+import com.example.test.springbootweb.config.TestTrace;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Testcontroller {
+@XSlf4j
 
+public class Testcontroller {
+    Logger  log = LoggerFactory.getLogger("Testcontroller");
     @GetMapping("/test")
+    @TestTrace
     public String test(){
+
+        log.info("this is test version v444444");
         return "hello,v444444";
     }
 }
